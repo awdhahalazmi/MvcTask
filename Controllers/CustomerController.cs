d3a0e4be5595974a8a1df447e7583c6b91da12ab
awdhah <awdah.almuharwal@gmail.com>
2024-04-16 10:20:00 +0300

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            Customer[] customers = new Customer[]
+   {
+        new Customer {Id = 1, Name = "awdhah",Email = "12@gmail.com",PhoneNumber = "1234"}, new Customer {Id = 2, Name = "fatmah",Email = "12@gmail.com",PhoneNumber = "1234"}
+   };
+            return View(customers.ToList());
+        }
+
+    }
+}
+
